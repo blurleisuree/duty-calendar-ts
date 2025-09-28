87d17a5d68431364bb5ef81c3f511e4967e18299
blurleisure <ipishir@gmail.com>
2025-09-29 01:32:40 +0300

    fix: lint autofix

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
-import { NextResponse } from 'next/server';
-
-import type { NextRequest } from 'next/server';
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   // request типизирован: можно использовать request.nextUrl.pathname, request.cookies и т.д.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,51 +2,51 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
- title: 'DutyCalendar',
-  description: 'Приложение для управления дежурствами',
-  manifest: '/manifest.json',
-  keywords: ['приложение', 'DutyCalendar', 'Управление дежурствами', 'PWA'],
-  authors: [{ name: 'blurleisure', url: 'https://github.com/blurleisuree' }],
+  title: "DutyCalendar",
+  description: "Приложение для управления дежурствами",
+  manifest: "/manifest.json",
+  keywords: ["приложение", "DutyCalendar", "Управление дежурствами", "PWA"],
+  authors: [{ name: "blurleisure", url: "https://github.com/blurleisuree" }],
   robots: {
     index: true,
     follow: true,
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   icons: {
-    icon: '/favicons/favicon.ico',
-    shortcut: '/favicons/favicon.ico',
-    apple: '/favicons/apple-touch-icon.png',
+    icon: "/favicons/favicon.ico",
+    shortcut: "/favicons/favicon.ico",
+    apple: "/favicons/apple-touch-icon.png",
     other: [
       {
-        rel: 'icon',
-        url: '/favicons/pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        rel: "icon",
+        url: "/favicons/pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        rel: 'icon',
-        url: '/favicons/pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        rel: "icon",
+        url: "/favicons/pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
   },
 };
 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1.0,
   maximumScale: 1.0,
   userScalable: false,
 };
 
-import Head from 'next/head';
+import Head from "next/head";
 // import { Header } from '@modules/Header/index.js';
 
 // обертка всего приложения (по типу App)
@@ -57,10 +57,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-       <Head>
+      <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
         <meta name="background-color" content="#ffffff" />
       </Head>
       <body>
